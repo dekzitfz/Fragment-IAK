4827cc033df6ae57a3e60b4446177c79c65ef844
Adi Andrea <dekz.itfz@gmail.com>
2017-08-05 10:33:55 +0800

    create fragment two

---
+++ app/src/main/java/id/dekz/fragmentiak/FragmentTwo.java
@@ -0,0 +1,23 @@
+package id.dekz.fragmentiak;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by DEKZ on 8/5/2017.
+ */
+
+public class FragmentTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_two, container, false);
+
+        return v;
+    }
+}
